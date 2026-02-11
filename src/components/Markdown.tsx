a80006b2b42155fb3050a71f06f03db310cb81c8
Joseph Irving <jirving0311@gmail.com>
2026-02-11 16:19:42 -0500

    Remove all rift-sense contents

--- src/components/Markdown.tsx
+++
@@ -1,7 +0,0 @@
-import DOMPurify from "isomorphic-dompurify";
-import { marked } from "marked";
-
-export function Markdown({ content }: { content: string }) {
-  const html = DOMPurify.sanitize(marked.parse(content));
-  return <div className="prose prose-slate max-w-none" dangerouslySetInnerHTML={{ __html: html }} />;
-}
